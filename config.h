81b37cc1626355385017484797d5cd70d1d71521
xrc2 <109512837+9xbt@users.noreply.github.com>
2024-06-27 13:16:07 +0200

    change screenshot script

--- config.h
+++ config.h
@@ -60,7 +60,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[]  = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]   = { "alacritty", NULL };
-static const char *yasscmd[]   = { "yass", "-c", NULL };
+static const char *yasscmd[]   = { "bash", "/home/xrc2/.screenshot.sh", NULL };
 static const char *incVolume[] = { "amixer", "-D", "pulse", "set", "Master", "2%+", NULL };
 static const char *decVolume[] = { "amixer", "-D", "pulse", "set", "Master", "2%-", NULL };
 
